c6793e2bf1df00d2feacc37e1f480c691d8619fd
Yan Chen Zhou <yche0094@uni.sydney.edu.au>
2025-02-24 12:16:23 +0100

    insert bibus

--- insert.sql
+++ insert.sql
@@ -1,4 +1,11 @@
-INSERT INTO Bibus VALUES();
+INSERT INTO Bibus (license_plate, last_itv, next_itv)
+    SELECT DISTINCT MIN(PLATE), MAX(TO_DATE(LAST_ITV, 'dd.mm.yyyy // HH24.MI.SS')), MIN(TO_DATE(NEXT_ITV, 'dd.mm.yyyy'))
+    FROM FSDB.BUSSTOPS
+    WHERE plate IS NOT NULL AND last_itv IS NOT NULL AND next_itv IS NOT NULL
+    GROUP BY PLATE;
+;
+
+INSERT INTO Bibusero VALUES();
 -- LIB_PASSPORT CHAR(20)
 -- LIB_EMAIL CHAR(100)
 -- LIB_FULLNAME CHAR(80)
@@ -6,8 +13,7 @@ INSERT INTO Bibus VALUES();
 -- LIB_PHONE CHAR(9)
 -- LIB_ADDRESS
 
-INSERT INTO Bibusero VALUES();
-INSERT INTO Municipality SELECT town, province, to_number(population) FROM fsdb.busstops;
+INSERT INTO Municipality SELECT DISTINCT town, province, to_number(population) FROM fsdb.busstops;
 INSERT INTO Books VALUES();
 
 INSERT INTO Route VALUES();
